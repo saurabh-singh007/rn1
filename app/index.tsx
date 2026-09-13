5631e595c8bc8612643f049282d50a38c50c116b
saurabh-singh007 <saurabhsinghthecreative@gmail.com>
2026-09-13 19:00:38 +0530

    fixed

--- app/index.tsx
+++ app/index.tsx
@@ -2,11 +2,7 @@ import { Redirect } from "expo-router";
 // import { useAuth } from "@/hooks/useAuth"; // however you track auth state
 
 export default function Index() {
-//   const { isLoggedIn, isLoading } = useAuth();
 
-//   if (isLoading) {
-//     return null; // or a splash/loading screen
-//   }
 
-  return  <Redirect href="/(tabs)" />;
+  return  <Redirect href="/(tabs)"  />;
 }
