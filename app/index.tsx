af218c37a8499dee2c76052d17409cf7fb103dd0
saurabh-singh007 <saurabhsinghthecool@gmail.com>
2026-09-13 19:19:07 +0530

    fix style

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,5 @@ import { Redirect } from "expo-router";
 // import { useAuth } from "@/hooks/useAuth"; // however you track auth state
 
 export default function Index() {
-
-
   return  <Redirect href="/(tabs)"  />;
 }
